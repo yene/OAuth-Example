0285e171d67b83979f261e6fe99a9ec8968edc20
yene <yene@slowmouse.ch>
2015-11-05 12:24:09 +0100

    requesting the token after sever has registered bonjour

--- OAuth Example/ViewController.swift
+++ OAuth Example/ViewController.swift
@@ -9,13 +9,24 @@
 import UIKit
 import GCDWebServers
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GCDWebServerDelegate {
+  
+  let webServer = GCDWebServer()
 
   @IBOutlet weak var instructionLabel: UILabel!
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // get request token
+    webServer.delegate = self
+    webServer.startWithPort(8080, bonjourName: "appletv")
+  }
+  
+  func webServerDidCompleteBonjourRegistration(server: GCDWebServer!) {
+    print("Visit \(webServer.bonjourServerURL) in your web browser")
+    instructionLabel.text = "Please Visit \(webServer.bonjourServerURL!.absoluteString) on another device and log in with your account."
+    requestToken()
+  }
+  
+  func requestToken() {
     let oauthswift = OAuth1Swift(
       consumerKey:    "5rqqWDzRnfXpvvbmP6qKw",
       consumerSecret: "LQbFp6O9niW1MlMr76CKqNHeJbAjzmyRwk2mbbq0",
@@ -26,46 +37,47 @@ class ViewController: UIViewController {
     
     let callbackURL = NSURL(string:"http://chobit.local:8080/")
     oauthswift.postOAuthRequestTokenWithCallbackURL(callbackURL!, success: { (credential, response) -> Void in
-      print("token is ", credential)
+      self.registerRedirect(credential.oauth_token)
       }) { (error) -> Void in
-        print("we faile beacuse ", error)
+        print("Error: ", error)
     }
-
-    
-    
-    let webServer = GCDWebServer()
-    
-    /*
-    docs https://www.goodreads.com/api/documentation#oauth
-    
-    1. request Goodreads for a request token.
-
-    2. redirect user to
-    http://www.goodreads.com/oauth/authorize?oauth_callback=http://chobit.local:8080/goodreads_oauth_callback&mobile=1&oauth_token=
-    
-
-    3. goodreads will give me the following request
-    http://yourapp.com/goodreads_oauth_callback?oauth_token=ezBHZc7C1SwvLGc646PEQ&authorize=1
-
-*/
-    
-    webServer.addDefaultHandlerForMethod("GET", requestClass: GCDWebServerRequest.self, processBlock: {request in
-      return GCDWebServerDataResponse(HTML:"<html><body><p>Hello World</p></body></html>")
-      
-    })
+  }
   
-    webServer.startWithPort(8080, bonjourName: "appletv")
+  func registerRedirect(token: String) {
+    let webserver = webServer.bonjourServerURL //NSURL(string: "http://chobit.local:8080/")
+    let authURL = "http://www.goodreads.com/oauth/authorize?oauth_callback=" + webserver!.absoluteString + "goodreads_oauth_callback&mobile=1&oauth_token=" + token
     
-    // Todo put into a callback
-    instructionLabel.text = "Please Visit http://chobit.local:8080/ on another device and log in with your account."
-    print("Visit \(webServer.serverURL) in your web browser")
+    // redirect / to auth page
+    webServer.addDefaultHandlerForMethod("GET", requestClass: GCDWebServerRequest.self, processBlock: {request in
+      return GCDWebServerDataResponse(HTML:"<html><body><p>Nothing here!</p></body></html>")
+    })
+    
+    webServer.addHandlerForMethod("GET", path: "/", requestClass: GCDWebServerRequest.self, processBlock: {request in
+      return GCDWebServerDataResponse(redirect: NSURL(string: authURL), permanent: true)
+      }
+    )
+    
+    // parse /goodreads_oauth_callback
+    webServer.addHandlerForMethod("GET", path: "/goodreads_oauth_callback", requestClass: GCDWebServerRequest.self, processBlock: {request in
+      
+      let oauthToken = request.query["oauth_token"] as! String
+      let success = request.query["authorize"] as! String
+      if (success == "1") {
+        dispatch_async(dispatch_get_main_queue(),{
+          self.instructionLabel.text = "Thank you for logging in. " + oauthToken
+        })
+        return GCDWebServerDataResponse(HTML:"<html><body><p>Thank you for logging in, you can now use the Apple TV</p></body></html>")
+      } else {
+        dispatch_async(dispatch_get_main_queue(),{
+          self.instructionLabel.text = "I am sorry there was an error."
+        })
+        return GCDWebServerDataResponse(HTML:"<html><body><p>I am sorry it did not work.</p></body></html>")
+      }
+    })
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
-
-
 }
 
